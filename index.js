6a8f23c8cf8ce74f767971614be04886f601268f
alexma-kunaico <alex.ma@kunaico.com>
2021-12-09 07:14:50 -0800

    Remove unused code

--- index.js
+++ index.js
@@ -4,7 +4,6 @@
 
 import 'react-native-gesture-handler';
 import { AppRegistry } from 'react-native';
-// import App from './App';
 import App from './src/Entrypoint';
 import { name as appName } from './app.json';
 import { enableScreens } from 'react-native-screens';
