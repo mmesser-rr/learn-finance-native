ac250f40295de980d096cf39d8ee4bacd2811e24
react-mmesser <michaelmesser0795@gmail.com>
2021-12-22 07:40:13 -0800

    Implement the screens for bank account intro and capture dob

---
+++ src/screens/CaptureDOB/index.tsx
@@ -0,0 +1,43 @@
+import React, { useState } from 'react';
+import { View } from 'react-native';
+
+import { Title } from 'src/components/common/Texts';
+import SubmitButton from 'src/components/common/SubmitButton';
+import AppLayout from 'src/components/layout/AppLayout';
+import TextInputMask from 'src/components/common/TextInputMask';
+import NavigationService from 'src/navigation/NavigationService';
+
+import styles from './styles';
+
+const CaptureDOB: React.FC = () => {
+  const [isValid, setIsValid] = useState(false);
+
+  const changeValue = (value: string) => {
+    setIsValid(!!value);
+  };
+
+  const goToNextStep = () => {};
+
+  return (
+    <AppLayout containerStyle={styles.container} viewStyle={styles.viewWrapper}>
+      <View style={styles.contentWrapper}>
+        <View>
+          <Title style={styles.head}>What's your date of birth?</Title>
+        </View>
+        <View>
+          <TextInputMask
+            label='Date of Birth (mm/dd/yyyy)'
+            mask='[00]/[00]/[0000]'
+            autoFocus={true}
+            changeValue={changeValue}
+          />
+        </View>
+      </View>
+      <View style={styles.actionWrapper}>
+        <SubmitButton isValid={isValid} actionLabel='Continue' onSubmit={goToNextStep} />
+      </View>
+    </AppLayout>
+  );
+};
+
+export default CaptureDOB;
