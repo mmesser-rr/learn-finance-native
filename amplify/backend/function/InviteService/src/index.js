211d9ba4992b9fe8ad019bf24c414e3928fbfdc8
ken <kenneth@kunaico.com>
2022-01-19 14:59:30 -0600

    fix function migration

--- amplify/backend/function/InviteService/src/index.js
+++ amplify/backend/function/InviteService/src/index.js
@@ -1,21 +1,19 @@
 /* Amplify Params - DO NOT EDIT
+	API_THEPLAYERSCOMPANY_GRAPHQLAPIENDPOINTOUTPUT
+	API_THEPLAYERSCOMPANY_GRAPHQLAPIIDOUTPUT
+	API_THEPLAYERSCOMPANY_GRAPHQLAPIKEYOUTPUT
+	API_THEPLAYERSCOMPANY_INVITETABLE_ARN
+	API_THEPLAYERSCOMPANY_INVITETABLE_NAME
 	ENV
 	REGION
-	API_THEPLAYERSCOMPANY_GRAPHQLAPIIDOUTPUT
-	API_THEPLAYERSCOMPANY_GRAPHQLAPIENDPOINTOUTPUT
-	API_THEPLAYERSCOMPANY_GRAPHQLAPIKEYOUTPUT
 Amplify Params - DO NOT EDIT */
 
-exports.handler = async (event) => {
-    // TODO implement
-    const response = {
-        statusCode: 200,
-    //  Uncomment below to enable CORS requests
-    //  headers: {
-    //      "Access-Control-Allow-Origin": "*",
-    //      "Access-Control-Allow-Headers": "*"
-    //  }, 
-        body: JSON.stringify('Hello from Lambda!'),
-    };
-    return response;
+const { generateAndPersistInvite } = require('./generateInvite.js');
+
+const resolvers = {
+  generateInvite: generateAndPersistInvite
 };
+
+const fallback = (event) => {throw new Error `No handler defined for fieldName: ${event.fieldName}`};
+
+exports.handler = async (event) => await (resolvers[event.fieldName] || fallback)(event);
