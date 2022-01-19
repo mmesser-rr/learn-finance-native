002163d3e5579cd7c3edd09607905cf98538f2df
ken <kenneth@kunaico.com>
2022-01-19 09:27:49 -0600

    complete initial infra

---
+++ windows/theplayerscompany/MainPage.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include "MainPage.g.h"
+#include <winrt/Microsoft.ReactNative.h>
+
+namespace winrt::theplayerscompany::implementation
+{
+    struct MainPage : MainPageT<MainPage>
+    {
+        MainPage();
+    };
+}
+
+namespace winrt::theplayerscompany::factory_implementation
+{
+    struct MainPage : MainPageT<MainPage, implementation::MainPage>
+    {
+    };
+}
+
