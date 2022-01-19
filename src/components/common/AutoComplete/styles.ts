3163c7bb7fa293e003a389181c927e8fdadccf5f
ken <kenneth@kunaico.com>
2022-01-19 17:40:57 -0600

    migrate frontend

---
+++ src/components/common/AutoComplete/styles.ts
@@ -0,0 +1,42 @@
+import { StyleSheet } from 'react-native';
+import AppColors from 'src/config/colors';
+import { scale } from 'src/config/dimentions';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  labelStyle: {
+    fontFamily: 'Lato',
+    color: AppColors.whiteColor,
+  },
+  searchStyle: {
+    backgroundColor: AppColors.grayColor,
+    color: AppColors.whiteColor,
+    borderRadius: scale(100),
+    height: scale(40)
+  },
+  searchInputStyle: {
+    fontFamily: 'Lato',
+    color: AppColors.whiteColor,
+    fontSize: scale(16)
+  },
+  selectWrapper: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    borderBottomWidth: 1,
+    borderColor: AppColors.placeholderColor,
+    paddingHorizontal: scale(16),
+    paddingVertical: scale(16),
+  },
+  selectLabel: {
+    color: AppColors.placeholderColor,
+  },
+  active: {
+    color: AppColors.whiteColor,
+    borderColor: AppColors.whiteColor,
+  },
+});
+
+export default styles;
---
+++ src/screens/SelectPlayerTag/styles.ts
@@ -0,0 +1,35 @@
+import { StyleSheet } from 'react-native';
+import AppColors from 'src/config/colors';
+import { scale } from 'src/config/dimentions';
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: AppColors.blackColor,
+  },
+  viewWrapper: {
+  },
+  head: {
+    marginBottom: scale(16),
+  },
+  caption: {
+    marginBottom: scale(32),
+  },
+  contentWrapper: {
+    flex: 1,
+  },
+  actionWrapper: {
+    flex: 1,
+  },
+  submit: {
+    marginTop: scale(-22),
+  },
+  textAffixColor: {
+    color: AppColors.whiteColor
+  },
+  left: {
+    fontSize: scale(16),
+    color: AppColors.whiteColor,
+  },
+});
+
+export default styles;
