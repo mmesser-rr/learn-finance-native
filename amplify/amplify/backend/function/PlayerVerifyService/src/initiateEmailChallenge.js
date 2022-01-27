7baa9f4772c9a45a2c16c96c5843e8c68e710e4d
Jenna Murrell <murrelljenna@gmail.com>
2022-01-27 14:04:58 -0500

    Initiate challenge against email

---
+++ amplify/amplify/backend/function/PlayerVerifyService/src/initiateEmailChallenge.js
@@ -0,0 +1,42 @@
+const AWS = require('aws-sdk');
+const ses = new AWS.SES();
+
+const paramsForEmail = (email, code) => ({
+  Destination: {
+    ToAddresses: [
+      'jenna.murrell@kunaico.com',
+    ]
+  },
+  Message: {
+    Body: {
+      Html: {
+       Charset: "UTF-8",
+       Data: code
+      },
+     },
+     Subject: {
+      Charset: 'UTF-8',
+      Data: 'Verify Your Email For The Players Company'
+     }
+    },
+  Source: 'jenna.murrell@kunaico.com',
+});
+
+const sendEmail = (params) => ses.sendEmail(params).promise();
+
+const sendEmailChallenge = (email) => {
+  const code = "HELLO";
+  const params = paramsForEmail(email, code);
+
+  return sendEmail(params);
+}
+
+const initiateEmailChallenge = (event) => {
+  const { email } = event.arguments;
+
+  return sendEmailChallenge(email);
+};
+
+module.exports = {
+  initiateEmailChallenge
+}
