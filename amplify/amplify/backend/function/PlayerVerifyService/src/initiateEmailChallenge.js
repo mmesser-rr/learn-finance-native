ff4b3efc33d4a6a0df1ac3122da05452d7fb8a99
Jenna Murrell <murrelljenna@gmail.com>
2022-01-31 10:22:11 -0500

    Create ses wrapper to mock in tests

--- amplify/amplify/backend/function/PlayerVerifyService/src/initiateEmailChallenge.js
+++ amplify/amplify/backend/function/PlayerVerifyService/src/initiateEmailChallenge.js
@@ -1,50 +1,10 @@
-const AWS = require('aws-sdk');
-const ses = new AWS.SES();
+const ses = require("./wrappers/sesWrapper.js");
 
 const api = require("./wrappers/emailChallengeGraphqlWrapper.js");
 
 const genCode = require("./genCode.js");
 
 const EmailChallenge = require("./models/EmailChallenge");
-
-const paramsForEmail = (email, code) => ({
-  Destination: {
-    ToAddresses: [
-      'jenna.murrell@kunaico.com',
-    ]
-  },
-  Message: {
-    Body: {
-      Html: {
-       Charset: "UTF-8",
-       Data: code
-      },
-     },
-     Subject: {
-      Charset: 'UTF-8',
-      Data: 'Verify Your Email For The Players Company'
-     }
-    },
-  Source: 'jenna.murrell@kunaico.com',
-});
-
-const sendEmail = (params) => ses.sendEmail(params).promise();
-
-const sendEmailChallenge = async (email) => {
-  const code = genCode();
-
-  const challengeToPersist = {
-    code: code,
-    email: email,
-    verified: false
-  }
-
-  const emailChallenge = await api.persistEmailChallenge(challengeToPersist);
-
-  const params = paramsForEmail(email, code);
-
-  return sendEmail(params);
-}
 
 const initiateEmailChallenge = async (event) => {
   const code = genCode();
@@ -58,7 +18,7 @@ const initiateEmailChallenge = async (event) => {
 
   const challenge = EmailChallenge(code, email);
 
-  return sendEmailChallenge(email).catch(err => {
+  return ses.sendEmailChallenge(email).catch(err => {
     throw new Error("Error sending email");
   })
     .then(d => api.persistEmailChallenge(challenge)).catch(err => {
