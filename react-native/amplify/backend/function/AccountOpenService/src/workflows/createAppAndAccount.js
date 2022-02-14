438755b34f9b335086b80cf0f0b936ea189301dc
Jenna Murrell <murrelljenna@gmail.com>
2022-02-14 12:56:25 -0500

    Get rid of over-embedded promises

--- react-native/amplify/backend/function/AccountOpenService/src/workflows/createAppAndAccount.js
+++ react-native/amplify/backend/function/AccountOpenService/src/workflows/createAppAndAccount.js
@@ -19,6 +19,16 @@ const keysFromUnit = (res) => {
   }
 };
 
+createAccountIfApproved = (athlete, res, keys) => {
+  const appStatus = res.data.attributes.status;
+  if  (appStatus === "Approved") {
+    return createAndPersistAccount(athleteWithUnitData(athlete, keys));
+  } else {
+    return Promise.reject(`Application to Unit is ${appStatus}. Please contact support.`);
+  }
+}
+
+
 const createAppAndAccount = (ssn, athlete) => {
   const custId = athlete?.unitLookup?.custId;
 
@@ -26,16 +36,14 @@ const createAppAndAccount = (ssn, athlete) => {
     throw new Error("Looks like this athlete is already affiliated with a Unit customer. Continuing will overwrite and lose current unit data. Bailing");
   }
 
-  return unit.createApplication(ssn, athlete).then(unitRes => {
-    const keys = keysFromUnit(unitRes)
-    return tpc.addUnitDataToAthlete(athlete.id, keys).then(tpcRes => {
-      const appStatus = unitRes.data.attributes.status;
-      if  (appStatus === "Approved") {
-        return createAndPersistAccount(athleteWithUnitData(athlete, keys));
-      } else {
-        return Promise.reject(`Application to Unit is ${appStatus}. Please contact support.`);
-      }
-    });
+  return unit.createApplication(ssn, athlete).then(res => {
+    const keys = keysFromUnit(res);
+    
+
+    return Promise.all([
+      tpc.addUnitDataToAthlete(athlete.id, keys),
+      createAccountIfApproved(athlete, res, keys)
+    ]);
   });
 }
 
