a3236dde3953862d670f55ef2a9303ff201dde28
react-mmesser <michaelmesser0795@gmail.com>
2022-02-16 08:07:34 -0800

    Implement first time user home screen

--- react-native/src/screens/Banking/Home/styles.ts
+++ react-native/src/screens/Banking/Home/styles.ts
@@ -6,8 +6,40 @@ const styles = StyleSheet.create({
   container: {
     backgroundColor: AppColors.coreBlack100,
   },
-  head: {
+  viewStyle: {
+    marginTop: 0,
+    marginBottom: 0,
+  },
+  dotMenu: {
+    marginVertical: scale(12),
+    alignItems: 'flex-end',
+  },
+  title: {
+    marginBottom: scale(44),
+  },
+  subTitle: {
     marginBottom: scale(16),
+  },
+  accountCard: {
+    marginBottom: scale(24),
+  },
+  card: {
+    paddingVertical: scale(12),
+    paddingHorizontal: scale(24),
+    borderRadius: scale(8),
+    backgroundColor: AppColors.coreBlack80,
+  },
+  cardHead: {
+    marginBottom: scale(4),
+  },
+  cardBody: {
+    marginBottom: scale(16),
+  },
+  depositButton: {
+    marginTop: scale(8),
+    backgroundColor: 'transparent',
+    borderWidth: 1,
+    borderColor: AppColors.gray20,
   },
   actionWrapper: {
     flex: 1,
