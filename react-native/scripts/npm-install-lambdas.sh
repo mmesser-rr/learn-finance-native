90e13081051e5454c4d75def87ebc930135f186b
ken <kenneth@kunaico.com>
2022-02-17 16:35:04 -0600

    update scripts

--- react-native/scripts/npm-install-lambdas.sh
+++ react-native/scripts/npm-install-lambdas.sh
@@ -5,9 +5,9 @@ for d in amplify/backend/function/* ; do
   echo Start Testing Function $d
   echo =================================================================
   echo ...Change directory
-  cd $rootDir/$d
+  cd $rootDir/$d/src
   echo $(pwd)
-  echo ...Instal packages
+  echo ...Install packages
   npm ci;    
   echo ...Run tests
   npm run test
