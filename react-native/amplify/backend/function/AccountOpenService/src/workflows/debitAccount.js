44e04328a4bf0d0a7597f1b5cb838e18c8468ab5
Debo Ajagbe <deboajagbe@Debos-MacBook-Pro.local>
2022-02-26 00:37:24 -0800

    account service update

--- react-native/amplify/backend/function/AccountOpenService/src/workflows/debitAccount.js
+++ react-native/amplify/backend/function/AccountOpenService/src/workflows/debitAccount.js
@@ -1,25 +1,25 @@
 const unit = require("../wrappers/unit");
 const tpc = require("../wrappers/tpc");
 
-const createDebitRequest = (athlete, amount, accountId, description, name, routingNumber, accountNumber, accountType) => {
+const createDebitRequest = (athleteId, amount, unitAccountId, description, receiverName, receiverRoutingNumber, receiverAccountNumber, receiverAccountType) => {
   const accounts = athlete?.accounts;
 
-  if (accounts.length !== 0 && accounts.unitAccountId.includes(accountId)) {
+  if (accounts.length !== 0 && accounts.unitAccountId.includes(unitAccountId)) {
        throw new Error("Looks like this athlete is using invalid banking");
   }
 
-  return unit.debitAccount(amount, accountId, description, name, routingNumber, accountNumber, accountType)
+  return unit.debitAccount(amount, unitAccountId, description,receiverName, receiverRoutingNumber, receiverAccountNumber, receiverAccountType)
     .catch(err => (err?.appId)
     )
 }
 
-const debitAccount = (athleteId, amount, accountId, description, name, routingNumber, accountNumber, accountType) => tpc.getAthlete(athleteId).then(athlete => 
+const debitAccount = (athleteId, amount, unitAccountId, description, receiverName, receiverRoutingNumber, receiverAccountNumber, receiverAccountType) => tpc.getAthlete(athleteId).then(athlete => 
   (athlete != null) ? 
-    createDebitRequest(athlete, amount, accountId, description, name, routingNumber, accountNumber, accountType) : 
+    createDebitRequest(athleteId, amount, unitAccountId, description, receiverName, receiverRoutingNumber, receiverAccountNumber, receiverAccountType) : 
     Promise.reject(`No athlete found with id ${athleteId}`)
 );
 
 module.exports.debitAccount = async (event) => {
-  const {athleteId, amount, accountId, description, name, routingNumber, accountNumber, accountType} = event.arguments;
-   return debitAccount(athleteId, amount, accountId, description, name, routingNumber, accountNumber, accountType)
+  const {athleteId, amount, unitAccountId, description, receiverName, receiverRoutingNumber, receiverAccountNumber, receiverAccountType} = event.arguments;
+   return debitAccount(athleteId, amount, unitAccountId, description, receiverName, receiverRoutingNumber, receiverAccountNumber, receiverAccountType)
 }
