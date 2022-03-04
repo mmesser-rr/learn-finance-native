b6526d1aba42e9e0912fc82dcf61c2b6d9c3e48b
alexma-kunaico <alex.ma@kunaico.com>
2022-03-04 07:17:17 -0800

    Implement pods setup and sucess screens

--- react-native/src/components/common/UserHomeModal/index.tsx
+++ react-native/src/components/common/UserHomeModal/index.tsx
@@ -4,8 +4,14 @@ import Modal from 'react-native-modal';
 
 import {Text} from 'src/components/common/Texts';
 import BackIcon from 'src/assets/icons/back.svg';
+import CancelIcon from 'src/assets/icons/cancel.svg';
+import DirectDepositIcon from 'src/assets/icons/direct-deposit.svg';
+import WithdrawGreyIcon from 'src/assets/icons/withdraw-grey.svg';
+import ChangePodGreyIcon from 'src/assets/icons/change-pod-allocations-grey.svg';
+import TransactionHistoryGreyIcon from 'src/assets/icons/transaction-history-grey.svg';
 
 import styles from './styles';
+import NavigationService from 'src/navigation/NavigationService';
 
 interface UserHomeModalProps {
   visible: boolean;
@@ -25,37 +31,42 @@ const UserHomeModal: React.FC<UserHomeModalProps> = ({visible, onClose}) => {
     onClose();
   };
 
+  const onSetupDirectDeposit = () => {
+    onClose();
+    NavigationService.navigate('TransferStack', {screen: 'DirectDeposit'});
+  };
+
   return (
     <Modal isVisible={isModalVisible} style={styles.modal}>
       <View style={styles.container}>
         <View style={styles.item}>
-          <TouchableOpacity style={styles.action}>
+          <TouchableOpacity style={styles.action} onPress={onSetupDirectDeposit}>
             <Text type="Body/Large">Set up direct deposit</Text>
-            <View><BackIcon /></View>
+            <View><DirectDepositIcon /></View>
           </TouchableOpacity>
         </View>
         <View style={styles.item}>
-          <TouchableOpacity style={styles.action}>
+          <TouchableOpacity style={styles.action} disabled>
             <Text type="Body/Large" style={styles.disabled}>Withdraw money</Text>
-            <View><BackIcon /></View>
+            <View><WithdrawGreyIcon /></View>
           </TouchableOpacity>
         </View>
         <View style={styles.item}>
-          <TouchableOpacity style={styles.action}>
+          <TouchableOpacity style={styles.action} disabled>
             <Text type="Body/Large" style={styles.disabled}>Change Pods allocations</Text>
-            <View><BackIcon /></View>
+            <View><ChangePodGreyIcon /></View>
           </TouchableOpacity>
         </View>
         <View style={styles.item}>
-          <TouchableOpacity style={styles.action}>
+          <TouchableOpacity style={styles.action} disabled>
             <Text type="Body/Large" style={styles.disabled}>Transaction History</Text>
-            <View><BackIcon /></View>
+            <View><TransactionHistoryGreyIcon /></View>
           </TouchableOpacity>
         </View>
         <View style={styles.item}>
           <TouchableOpacity style={styles.action} onPress={handleClose}>
             <Text type="Body/Large">Cancel</Text>
-            <View><BackIcon /></View>
+            <View><CancelIcon /></View>
           </TouchableOpacity>
         </View>
       </View>
