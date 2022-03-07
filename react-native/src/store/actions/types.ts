6f560a35f642f7bc2c1233516094946edb77a2d8
Ross Acheson <rossacheson@users.noreply.github.com>
2022-03-07 11:26:23 -0600

    Add onboarding reducer

--- react-native/src/store/actions/types.ts
+++ react-native/src/store/actions/types.ts
@@ -5,13 +5,15 @@ export const LOGIN_DISABLE_LOADER = 'LOGIN_DISABLE_LOADER';
 // theming
 export const TOGGLE_THEME = 'TOGGLE_THEME';
 
+// onboarding
+export const ONBOARDING_UPDATE = 'ONBOARDING_UPDATE';
+
 // login
 export const LOGIN_REQUEST = 'LOGIN_REQUEST';
 export const LOGIN_RESPONSE = 'LOGIN_RESPONSE';
 export const LOGIN_LOADING_ENDED = 'LOGIN_LOADING_ENDED';
 export const LOGIN_FAILED = 'LOGIN_FAILED';
 export const LOG_OUT = 'LOG_OUT';
-
 
 // banking
 export const UPDATE_HOME_STEP = 'UPDATE_HOME_STEP';
---
+++ react-native/src/store/reducers/onboardingReducer.ts
@@ -0,0 +1,28 @@
+/* Onboarding Reducer
+ * handles state during onboarding
+ */
+import createReducer from 'src/lib/createReducer';
+import * as types from 'src/store/actions/types';
+
+import {IOnboardingState} from 'src/models/reducers/onboarding';
+import {IOnboardingUpdate} from 'src/models/actions/onboarding';
+
+const initialState: IOnboardingState = {
+  firstName: '',
+  lastName: '',
+  mobilePhone: '',
+  email: '',
+  ssn: '',
+};
+
+export const onboardingReducer = createReducer(initialState, {
+  [types.ONBOARDING_UPDATE](
+    state: IOnboardingState,
+    action: IOnboardingUpdate,
+  ) {
+    return {
+      ...state,
+      ...action.update,
+    };
+  },
+});
