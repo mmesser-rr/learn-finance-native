761be664b2029fc227b716c87ae6404372c14442
Ross Acheson <rossacheson@users.noreply.github.com>
2022-03-07 18:25:29 -0600

    Getting ready to call create athlete

--- react-native/src/store/reducers/loadingReducer.ts
+++ react-native/src/store/reducers/loadingReducer.ts
@@ -7,10 +7,17 @@ import * as types from 'src/store/actions/types';
 import {ILoading} from 'src/models/reducers/loading';
 
 const initialState: ILoading = {
+  isLoading: false,
   isLoginLoading: false,
 };
 
 export const loadingReducer = createReducer(initialState, {
+  [types.ENABLE_LOADER](state: ILoading) {
+    return {...state, isLoading: true};
+  },
+  [types.DISABLE_LOADER](state: ILoading) {
+    return {...state, isLoading: false};
+  },
   [types.LOGIN_ENABLE_LOADER](state: ILoading) {
     return {...state, isLoginLoading: true};
   },
