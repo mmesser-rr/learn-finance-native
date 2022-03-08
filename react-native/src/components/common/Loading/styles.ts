c657fca1e950a6ab0011f1c10938c34d7f6ccc3d
Ross Acheson <rossacheson@users.noreply.github.com>
2022-03-07 18:03:53 -0600

    Add Loading component

---
+++ react-native/src/components/common/Loading/styles.ts
@@ -0,0 +1,24 @@
+import {Dimensions, StyleSheet} from 'react-native';
+import AppColors from 'src/config/colors';
+
+const styles = StyleSheet.create({
+  backdrop: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    width: '100%',
+    height: '100%',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: AppColors.backdrop,
+  },
+  spinnerContainer: {
+    transform: [{translateY: -56}],
+  },
+  spinner: {
+    height: 56,
+    width: 56,
+  },
+});
+
+export default styles;
