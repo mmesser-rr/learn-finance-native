08faed46b090657c3eb87f1b841b39472040f69b
alexma-kunaico <alex.ma@kunaico.com>
2022-03-09 00:22:28 -0800

    Implement pods setup

--- react-native/src/screens/Banking/Home/index.tsx
+++ react-native/src/screens/Banking/Home/index.tsx
@@ -9,16 +9,20 @@ import {useDispatch, useSelector} from 'react-redux';
 import AppLayout from 'src/components/layout/AppLayout';
 import Button from 'src/components/common/Button';
 import {Text} from 'src/components/common/Texts';
-import {PodsCardGradient} from 'src/utils/constants';
+import {PodsCardGradient, PODsSteps} from 'src/utils/constants';
 import ThreeDotsIcon from 'src/assets/icons/three-dots.svg';
 import NavigationService from 'src/navigation/NavigationService';
 import {createLinkToken, getExistingLinkToken} from 'src/services/plaid';
-import {RootState} from 'src/store/root-state';
-import {generateTextStyle} from 'src/utils/functions';
-import {updateHomeStep} from 'src/store/actions/bankingActions';
+import { RootState } from 'src/store';
+import { generateTextStyle } from 'src/utils/functions';
+import UserHomeModal from 'src/components/common/UserHomeModal';
+import ProcessingIcon from 'src/assets/icons/processing.svg';
+import SpendingIcon from 'src/assets/icons/spending.svg';
+import InvestmentIcon from 'src/assets/icons/investment.svg';
+import SavingIcon from 'src/assets/icons/saving.svg';
+import SwitchIcon from 'src/assets/icons/switch.svg';
 
 import styles from './styles';
-import UserHomeModal from 'src/components/common/UserHomeModal';
 
 interface CardProps {
   style?: TextStyle;
@@ -40,17 +44,12 @@ const Card: React.FC<CardProps> = ({active, children, style: propsStyle}) => {
 };
 
 const Home: React.FC = () => {
-  const dispatch = useDispatch();
-  const {step} = useSelector((state: RootState) => state.bankingReducer);
+  const { step } = useSelector((state: RootState) => state.bankingReducer);
   const [linkToken, setLinkToken] = useState('');
   const [isModalVisible, setModalVisible] = useState(false);
 
   useEffect(() => {
     getPlaidLinkToken();
-
-    return () => {
-      dispatch(updateHomeStep('account'));
-    };
   }, []);
 
   const getPlaidLinkToken = async () => {
@@ -89,8 +88,6 @@ const Home: React.FC = () => {
     setModalVisible(true);
   };
 
-  const isPods = step === 'pods';
-
   return (
     <AppLayout containerStyle={styles.container} viewStyle={styles.viewStyle}>
       <View style={styles.dotMenu}>
@@ -101,10 +98,20 @@ const Home: React.FC = () => {
       <View style={styles.title}>
         <Text type="Headline/Small">Hi John!</Text>
       </View>
+      <View style={styles.submitted}>
+        {step === PODsSteps[2] && (
+          <>
+            <ProcessingIcon />
+            <Text type="Title/Small" style={styles.submittedLabel}>
+              1 transfer is submitted
+            </Text>
+          </>
+        )}
+      </View>
       <View style={styles.subTitle}>
         <Text type="Title/Medium">PLAYER'S ACCOUNT</Text>
       </View>
-      <Card active={!isPods} style={styles.accountCard}>
+      <Card active={step === PODsSteps[0]} style={styles.accountCard}>
         <View style={styles.cardHead}>
           <Text type="Headline/Small">Add money to your account</Text>
         </View>
@@ -116,11 +123,7 @@ const Home: React.FC = () => {
         <View>
           {!!linkToken && (
             <View>
-              {isPods ? (
-                <Button disabled>
-                  <Text type="Body/Large">Transfer from another bank</Text>
-                </Button>
-              ) : (
+              {step === PODsSteps[0] ? (
                 <PlaidLink
                   tokenConfig={{
                     token: linkToken,
@@ -136,14 +139,15 @@ const Home: React.FC = () => {
                     </Text>
                   </View>
                 </PlaidLink>
+              ) : (
+                <Button disabled>
+                  <Text type="Body/Large">Transfer from another bank</Text>
+                </Button>
               )}
             </View>
           )}
           <View>
-            <Button
-              actionStyle={styles.deposit}
-              onPress={onSetupDirectDeposit}
-              disabled={isPods}>
+            <Button actionStyle={styles.deposit} onPress={onSetupDirectDeposit} disabled={step !== PODsSteps[0]}>
               <Text type="Body/Large">Set up direct deposit</Text>
             </Button>
           </View>
@@ -151,27 +155,70 @@ const Home: React.FC = () => {
       </Card>
       <View style={styles.subTitle}>
         <Text type="Title/Medium">PODS</Text>
+        {step === PODsSteps[2] && (
+          <View style={styles.switch}>
+            <SwitchIcon />
+            <Text type="Body/Medium" style={styles.switchLabel}>Move Money</Text>
+          </View>
+        )}
       </View>
-      <Card active={isPods}>
-        <View style={styles.cardHead}>
-          <Text type="Headline/Small">Let's set up your pods</Text>
-        </View>
-        <View style={styles.cardBody}>
-          <Text type="Body/Large">
-            All incoming payments will be divided among your pods. Learn how
-            pods can help you financially.
-          </Text>
-        </View>
-        <View>
+      {step !== PODsSteps[2] && (
+        <Card active={step === PODsSteps[1]}>
+          <View style={styles.cardHead}>
+            <Text type="Headline/Small">Let's set up your pods</Text>
+          </View>
+          <View style={styles.cardBody}>
+            <Text type="Body/Large">
+              All incoming payments will be divided among your pods. Learn how
+              pods can help you financially.
+            </Text>
+          </View>
           <View>
-            <Button onPress={onSetupPods} disabled={!isPods}>
-              <Text type="Body/Large">Set up Pods</Text>
-            </Button>
+            <View>
+              <Button onPress={onSetupPods} disabled={step !== PODsSteps[1]}>
+                <Text type="Body/Large">Set up Pods</Text>
+              </Button>
+            </View>
+          </View>
+        </Card>
+      )}
+      {step === PODsSteps[2] && (
+        <View>
+          <View style={styles.podItem}>
+            <View style={styles.podLabel}>
+              <SpendingIcon />
+              <Text type="Body/Large" style={styles.podLabelText}>Spending</Text>
+            </View>
+            <View>
+              <Text type="Title/Small" style={styles.balanceLabel}>Balance</Text>
+              <Text type="Headline/Small">$0.00</Text>
+            </View>
+          </View>
+          <View style={styles.podItem}>
+            <View style={styles.podLabel}>
+              <InvestmentIcon />
+              <Text type="Body/Large" style={styles.podLabelText}>Investments</Text>
+            </View>
+            <View>
+              <Text type="Title/Small" style={styles.balanceLabel}>Balance</Text>
+              <Text type="Headline/Small">$0.00</Text>
+            </View>
+          </View>
+          <View style={styles.podItem}>
+            <View style={styles.podLabel}>
+              <SavingIcon />
+              <Text type="Body/Large" style={styles.podLabelText}>Saving</Text>
+            </View>
+            <View>
+              <Text type="Title/Small" style={styles.balanceLabel}>Balance</Text>
+              <Text type="Headline/Small">$0.00</Text>
+            </View>
           </View>
         </View>
-      </Card>
+      )}
       <UserHomeModal
         visible={isModalVisible}
+        step={step}
         onClose={() => setModalVisible(false)}
       />
     </AppLayout>
