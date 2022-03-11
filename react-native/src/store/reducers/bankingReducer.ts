3eff93dbb02db90489688ebfbfd7eadb8d947ac6
Ross Acheson <rossacheson@users.noreply.github.com>
2022-03-10 18:33:55 -0600

    Persist pod settings changes to backend

--- react-native/src/store/reducers/bankingReducer.ts
+++ react-native/src/store/reducers/bankingReducer.ts
@@ -1,17 +1,23 @@
 import createReducer from 'src/lib/createReducer';
 
-import {IThemeState} from 'src/models/reducers/theme';
-import { UPDATE_HOME_STEP } from '../actions/types';
-import { IUpdateHomeStepAction } from 'src/models/actions/banking';
-import { IBankingState } from 'src/models/reducers/banking';
-import { PODsSteps } from 'src/utils/constants';
+import {POD_SETTINGS_UPDATED, UPDATE_HOME_STEP} from '../actions/types';
+import {
+  IPodSettingsUpdated,
+  IUpdateHomeStepAction,
+} from 'src/models/actions/banking';
+import {IBankingState} from 'src/models/reducers/banking';
+import {PODsSteps} from 'src/utils/constants';
 
 const initialState: IBankingState = {
   step: PODsSteps[0],
+  podSettingsUpdated: false,
 };
 
 export const bankingReducer = createReducer(initialState, {
-  [UPDATE_HOME_STEP](state: IThemeState, action: IUpdateHomeStepAction) {
+  [UPDATE_HOME_STEP](state: IBankingState, action: IUpdateHomeStepAction) {
     return {...state, step: action.step};
+  },
+  [POD_SETTINGS_UPDATED](state: IBankingState, action: IPodSettingsUpdated) {
+    return {...state, podSettingsUpdated: action.updated};
   },
 });
