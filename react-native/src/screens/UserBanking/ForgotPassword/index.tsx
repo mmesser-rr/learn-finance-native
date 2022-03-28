5fed19676b5b82525f652d95acbd0075d04e99f3
alexma-kunaico <alex.ma@kunaico.com>
2022-03-28 08:09:47 -0700

    Update forgot password and onboarding

--- react-native/src/screens/UserBanking/ForgotPassword/index.tsx
+++ react-native/src/screens/UserBanking/ForgotPassword/index.tsx
@@ -7,19 +7,25 @@ import TopNav from 'src/components/common/TopNav';
 import Loading from 'src/components/common/Loading';
 import ForgotPasswordRequest from './ForgotPasswordRequest';
 import Verification from './Verification';
+import CaptureSSN from './CaptureSSN';
+import ResetPassword from './ResetPassword';
 
 import styles from './styles';
 
 const steps = [
   'request',
-  'verification'
+  'verification',
+  'ssn',
+  'reset_password'
 ];
 
 const ForgotPassword: React.FC = () => {
   const [loading, setLoading] = useState(false);
+  const [isPhoneVerification, setIsPhoneVerification] = useState(true);
   const [step, setStep] = useState(steps[0]);
   const [username, setUsername] = useState('');
   const [code, setCode] = useState('');
+  const [ssn, setSsn] = useState('');
 
   const goToLogin = () => {
     NavigationService.navigate('UserLoginStack', {screen: 'UserLogin'});
@@ -39,14 +45,32 @@ const ForgotPassword: React.FC = () => {
           updateLoading={setLoading}
           goToNextStep={() => setStep(steps[1])}
           updateUsername={setUsername}
+          updatePhoneVerification={setIsPhoneVerification}
         />
       )}
       {step === steps[1] && (
         <Verification
           username={username}
+          isPhoneVerification={isPhoneVerification}
           updateLoading={setLoading}
           updateCode={setCode}
-          goToNextStep={() => {}}
+          goToNextStep={() => setStep(steps[2])}
+        />
+      )}
+      {step === steps[2] && (
+        <CaptureSSN
+          updateLoading={setLoading}
+          updateSSN={setSsn}
+          goToNextStep={() => setStep(steps[3])}
+        />
+      )}
+      {step === steps[3] && (
+        <ResetPassword
+          username={username}
+          code={code}
+          isPhoneVerification={isPhoneVerification}
+          updateLoading={setLoading}
+          goToNextStep={() => goToLogin()}
         />
       )}
       {loading && <Loading />}
