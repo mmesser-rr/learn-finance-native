8935a49ab25c0394856dcd17dece2033dbe5edb6
Ross Acheson <rossacheson@users.noreply.github.com>
2022-04-05 10:35:32 -0500

    WIP on TPC-100 and Rewards screen

--- react-native/src/navigation/WyreStack.tsx
+++ react-native/src/navigation/WyreStack.tsx
@@ -9,6 +9,7 @@ import RewardsAccountOpened from 'src/screens/Wyre/RewardsAccountOpened';
 import PurchaseAmount from 'src/screens/Wyre/PurchaseAmount';
 import PurchaseReview from 'src/screens/Wyre/PurchaseReview';
 import PurchaseSubmitted from 'src/screens/Wyre/PurchaseSubmitted';
+import Rewards from 'src/screens/Wyre/Rewards';
 
 const Stack = createStackNavigator();
 
@@ -29,6 +30,7 @@ const WyreStack: React.FC = () => {
       <Stack.Screen name="PurchaseAmount" component={PurchaseAmount} />
       <Stack.Screen name="PurchaseReview" component={PurchaseReview} />
       <Stack.Screen name="PurchaseSubmitted" component={PurchaseSubmitted} />
+      <Stack.Screen name="Rewards" component={Rewards} />
     </Stack.Navigator>
   );
 };
