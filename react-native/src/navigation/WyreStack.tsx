129ecc8949515e5aea35e2f1d8ec8076804a76dc
Ross Acheson <rossacheson@users.noreply.github.com>
2022-04-04 22:01:59 -0500

    TPC-95 purchase review

--- react-native/src/navigation/WyreStack.tsx
+++ react-native/src/navigation/WyreStack.tsx
@@ -8,6 +8,7 @@ import OpenRewardsAccount from 'src/screens/Wyre/OpenRewardsAccount';
 import RewardsAccountOpened from 'src/screens/Wyre/RewardsAccountOpened';
 import PurchaseAmount from 'src/screens/Wyre/PurchaseAmount';
 import PurchaseReview from 'src/screens/Wyre/PurchaseReview';
+import PurchaseSubmitted from 'src/screens/Wyre/PurchaseSubmitted';
 
 const Stack = createStackNavigator();
 
@@ -27,6 +28,7 @@ const WyreStack: React.FC = () => {
       />
       <Stack.Screen name="PurchaseAmount" component={PurchaseAmount} />
       <Stack.Screen name="PurchaseReview" component={PurchaseReview} />
+      <Stack.Screen name="PurchaseSubmitted" component={PurchaseSubmitted} />
     </Stack.Navigator>
   );
 };
