c05e0bf8589551058cca788ec545b7271cf44eab
react-mmesser <michaelmesser0795@gmail.com>
2022-08-09 20:17:10 -0700

    made learn module workable except video and status

--- react-native/src/navigation/OpportunitiesStack.tsx
+++ react-native/src/navigation/OpportunitiesStack.tsx
@@ -13,9 +13,9 @@ const OpportunitiesStack: React.FC = () => {
   return (
     <Stack.Navigator initialRouteName="Opportunities">
       
-      <Stack.Screen name="Opportunities" component={Opportunities} />
-      <Stack.Screen name="LearnVideo" component={LearnVideo} />
-      <Stack.Screen name="Exercise" component={Exercise} initialParams={{started: false}} />
+      <Stack.Screen name="Opportunities" component={Opportunities} options={{ headerShown: false }} />
+      <Stack.Screen name="LearnVideo" component={LearnVideo} options={{ headerShown: false }} />
+      <Stack.Screen name="Exercise" component={Exercise} initialParams={{started: false}} options={{ headerShown: false }} />
       <Stack.Screen name="Redeem" component={Redeem} options={{ headerShown: false }} />
       <Stack.Screen name="AboutEvent" component={AboutEvent} options={{ headerShown: false }} />
 
